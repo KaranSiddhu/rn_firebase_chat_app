8061322cfe22ad60b6bfb2892593504c80368ad5
KaranSiddhu <karan@maguresoftwares.com>
2022-11-23 15:49:40 +0530

    changed firebase config

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyAl3L0bODT3PCuZ7Tj8Vp9BmQu4iNUTCRg',
-	authDomain: 'rnchatapp-45c9d.firebaseapp.com',
-	projectId: 'rnchatapp-45c9d',
-	storageBucket: 'rnchatapp-45c9d.appspot.com',
-	messagingSenderId: '286647957702',
-	appId: '1:286647957702:web:2399184f666932d0abffc7'
+	//GET CONFIG FROM FIREBASE
 };
 
 let app;
